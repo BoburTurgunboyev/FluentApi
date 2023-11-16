26f24f63d5ef3a55f36f40e200169c50cefaf4c0
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-16 17:53:50 +0500

    ozgarish

---
+++ FluentAPI/Configrations/UsersConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using FluentAPI.Entities;
+
+namespace FluentAPI.Configrations
+{
+    public class UsersConfiguration : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.FirstName)
+                .HasMaxLength(30)
+                .HasDefaultValue("Unknown")
+                .IsRequired();
+
+            builder.Property(x => x.LastName)
+                .HasMaxLength(30)
+                .HasDefaultValue("Unknown")
+                .IsRequired();
+
+            builder.HasOne(x => x.Propiska)
+                .WithOne(x => x.User)
+                .HasForeignKey<Propiska>(x => x.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x =>x.Home)
+                .WithOne(x =>x.User)
+                .HasForeignKey(x => x.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
+
+
+
