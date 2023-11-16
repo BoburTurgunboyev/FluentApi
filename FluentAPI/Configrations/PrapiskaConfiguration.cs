dec26207639b854b3efbc07cfe2aa46ee68ca66f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-16 17:53:58 +0500

    ozgarish

---
+++ FluentAPI/Configrations/PrapiskaConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FluentAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentAPI.Configrations
+{
+    public class PrapiskaConfiguration : IEntityTypeConfiguration<Propiska>
+    {
+        public void Configure(EntityTypeBuilder<Propiska> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.GivenDay)
+                .HasMaxLength(50)
+                .HasDefaultValue("2023")
+                .IsRequired();
+
+            builder.HasOne(builder => builder.User)
+                .WithOne(builder => builder.Propiska)
+                .HasForeignKey<Users>(x => x.Id)
+                .OnDelete(DeleteBehavior.Cascade);   
+        }
+    }
+
+}
