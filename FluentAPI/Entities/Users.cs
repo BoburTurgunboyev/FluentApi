c19c25cdad91b6b3f441b131d67a8ab2c8c554c3
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-16 17:53:34 +0500

    ozgarish

--- FluentAPI/Entities/Users.cs
+++ FluentAPI/Entities/Users.cs
@@ -5,14 +5,12 @@ namespace FluentAPI.Entities
     public class Users
     {
         public int Id {  get; set; }
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
         [ForeignKey("PropiskaId")]
         public Propiska Propiska { get; set; }
 
-        [ForeignKey("HomeID")]
         public ICollection<Homes> Home { get; set; }   
 
         
