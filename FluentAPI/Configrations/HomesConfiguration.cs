6a3ac7ce45e45f5c825d581c5572ab5e5aa4b723
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-16 17:54:02 +0500

    ozgarish

---
+++ FluentAPI/Configrations/HomesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using FluentAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentAPI.Configrations
+{
+    public class HomesConfiguration : IEntityTypeConfiguration<Homes>
+    {
+        public void Configure(EntityTypeBuilder<Homes> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+
+            builder.Property(x => x.Location)
+                .HasMaxLength(50)
+                .HasDefaultValue("Angren")
+                .IsRequired();
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.Home)
+                .HasForeignKey(x => x.Id)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
