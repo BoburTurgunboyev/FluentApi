16bd4ba0a88be8a5ba88e55d65c8656e446a4c28
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-16 19:38:41 +0500

    change

---
+++ FluentAPI/Migrations/20231116133606_First.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FluentAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false, defaultValue: "Unknown"),
+                    LastName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false, defaultValue: "Unknown")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "homes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Location = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, defaultValue: "Angren"),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_homes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_homes_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "propiska",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GivenDay = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, defaultValue: "2023"),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_propiska", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_propiska_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_homes_UserId",
+                table: "homes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_propiska_UserId",
+                table: "propiska",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "homes");
+
+            migrationBuilder.DropTable(
+                name: "propiska");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
